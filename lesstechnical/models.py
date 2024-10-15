1bee9eae6ff45686e885b7a7ad5a9d28ac361221
mattdoestech <mattdoesattractivetech@outlook.com>
2024-10-14 22:30:26 -0400

    add tables for posts and topics

--- lesstechnical/models.py
+++ lesstechnical/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    topic = models.CharField(max_length=200)
+    created = models.DateTimeField("date created")
+    published = models.DateTimeField("date published")
+    picture = models.URLField
+    author = models.CharField(max_length=30, default='Matt') 
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    less_tech_text = models.TextField
+    more_tech_text = models.TextField
+
